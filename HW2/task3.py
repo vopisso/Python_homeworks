b10aad2f3aa7d821fbd6c6071b5f29fbdbcaab87
Oleg Osipov <vopisso@gmail.com>
2022-09-05 23:05:15 +0300

    Task #3 is done

---
+++ HW2/task3.py
@@ -0,0 +1,15 @@
+# Требуется найти наименьший натуральный делитель целого числа N, отличный от 1.
+
+
+def find_minimal_divider(num):
+    if num < 1 or num > 10 ** 6:
+        return 'Incorrect input'
+    else:
+        i = 1
+        while i <= num:
+            i += 1
+            if num % i == 0:
+                return f'The minimal divider is {i}'
+
+number = int(input('Enter number: '))
+print(find_minimal_divider(number))
