fd9d02bcef75f9f65399a207a2fdf98e1ee04550
Oleg Osipov <vopisso@gmail.com>
2022-09-30 18:27:42 +0300

    Задача 29: переименован параметр функции

--- HW6/task29.py
+++ HW6/task29.py
@@ -1,8 +1,8 @@
-def find_farthest_orbit(tuple):
+def find_farthest_orbit(tup):
     pi = 3.14
     list1 = []
     result = ()
-    for i in tuple:
+    for i in tup:
         a, b = i
         if a != b:
             list1.append(a)
@@ -12,6 +12,7 @@ def find_farthest_orbit(tuple):
         if pi * list1[i] * list1[i + 1] == max_s:
             result = list1[i], list1[i + 1]
     return result
+
 
 orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
 print(*find_farthest_orbit(orbits))
