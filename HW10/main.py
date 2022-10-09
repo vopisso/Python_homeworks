023b3fa4ba3f25cd3a336e4ffb1f6975460fd6c5
Oleg Osipov <vopisso@gmail.com>
2022-10-09 19:45:48 +0300

    ok

--- HW10/main.py
+++ HW10/main.py
@@ -4,8 +4,8 @@ from scripts import check
 from random import choice as ch
 
 
-bot = Bot(token='5717925844:AAE1IjA_6FmDoiy0deyrJVoqRRZzNY3ueGo')
-updater = Updater(token='5717925844:AAE1IjA_6FmDoiy0deyrJVoqRRZzNY3ueGo')
+bot = Bot(token='')
+updater = Updater(token='')
 dispatcher = updater.dispatcher
 
 data = {6: 4, 7: 4, 8: 4, 9: 4, 10: 4, 'Валет': 4, 'Дама': 4, 'Король': 4,
